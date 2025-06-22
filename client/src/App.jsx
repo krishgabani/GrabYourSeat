03a7e9e60b5ee8fe930b7338cff0e90b52d8da71
Krish-Gabani <krishgabani7@gmail.com>
2025-06-22 13:00:04 +0530

    feat: admin layout, sidebar, navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,40 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import MovieDetails from "./pages/MovieDetails";
-import SeatLayout from "./pages/SeatLayout";
-import MyBookings from "./pages/MyBookings";
-import Favorite from "./pages/Favorite";
-import { Toaster } from "react-hot-toast";
-import Footer from "./components/Footer";
+import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favorite from './pages/Favorite';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
+import Layout from './pages/admin/Layout';
+import Dashboard from './pages/admin/Dashboard';
+import AddShows from './pages/admin/AddShows';
+import ShowsList from './pages/admin/ShowsList';
+import BookingsList from './pages/admin/BookingsList';
 
 function App() {
-  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
 
   return (
     <>
       <Toaster />
       {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieDetails />} />
-        <Route path="/movies/:id/:date" element={<SeatLayout />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/favorite" element={<Favorite />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-shows' element={<AddShows />} />
+          <Route path='shows-list' element={<ShowsList />} />
+          <Route path='bookings-list' element={<BookingsList />} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
