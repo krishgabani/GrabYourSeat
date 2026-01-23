ad9e57e83ea5d0237906430b961b66f00ad5a7aa
Krish Gabani <krishgabani7@gmail.com>
2026-01-23 16:04:27 +0530

    migration file isPaid

---
+++ server/prisma/migrations/20260123102000_removed_is_paid_booking/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_paid` on the `bookings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "bookings" DROP COLUMN "is_paid";
